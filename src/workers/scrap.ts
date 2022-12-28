be2953790107f0ec60b457fe58e5e56cff2beae5
mihailshumilov <mschumilow@gmail.com>
2022-12-28 21:04:42 +0200

    add puppeeteer extra usage

--- src/workers/scrap.ts
+++ src/workers/scrap.ts
@@ -1,22 +1,30 @@
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-import puppeteer from 'puppeteer';
+import {executablePath} from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import {Worker, Job} from 'bullmq';
 import axios from 'axios';
 
 
 (async () => {
-    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
+    puppeteer.use(StealthPlugin());
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox'],
+        defaultViewport: null,
+        executablePath: executablePath(),
+    });
 
     const worker = new Worker(process.env.SCRAP_QUEUE, async (job: Job) => {
             const page = await browser.newPage();
-            await page.setViewport({
-                width: 1440,
-                height: 900
-            });
+            // await page.setViewport({
+            //     width: 1440,
+            //     height: 900
+            // });
             await page.goto(job.data.url, {waitUntil: 'domcontentloaded', timeout: 30000});
-            // TODO: possible we need some 10-20 sec timeout here
+            await page.waitForTimeout(10*1000);
             const data = await page.evaluate(() => document.querySelector('*').outerHTML);
             await page.close();
             console.log('start callback: ', job.data.callback);
